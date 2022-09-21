4cd76fd276604ed54bfcc7f573796fcf690c0453
Anastasya <gavrilovaanastasya@yandex.ru>
2022-09-21 14:01:08 +0300

    Program four: вывести последнее число 3х значного числа

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿// Программа, принимающая на вход 3-х значное число, а на выходе показывает последнюю цифру этого числа
+/*Попыталась написать программу, не получается присвоить переменные каждому по очереди введенному числу.
+ Не получается это сделать*/
+
+
+Console.WriteLine("Введите 3-значное число: ");
+int numbers = int.Parse(Console.ReadLine());
+
+int one = int.Parse(Console.ReadLine());
+int two = int.Parse(Console.ReadLine());
+int tree = int.Parse(Console.ReadLine());
+int number1 = one;
+int number2 = two;
+int number3 = tree;
+numbers = int.Parse($"Console.ReadLine{one}{two}{tree}");
+
+Console.WriteLine(tree);
